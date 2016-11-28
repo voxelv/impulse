098f2ab0e7aba71172ef746f50624499e9ae99d5
Tim Slippy, voxelv <slippytim@gmail.com>
2016-11-28 00:55:29 -0800

    initial commit

---
+++ android/src/com/derelictech/impulse/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.derelictech.impulse;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.derelictech.impulse.Impulse;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Impulse(), config);
+	}
+}
