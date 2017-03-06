8be963c7140ab7895214360b24bac4707075b20e
Tim Slippy, voxelv <slippytim@gmail.com>
2017-03-05 22:35:19 -0800

    validatable texfield, ESC loose-focus on text field

--- core/src/com/derelictech/impulse/MainGameScreen.java
+++ core/src/com/derelictech/impulse/MainGameScreen.java
@@ -5,9 +5,12 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.derelictech.impulse.util.StringUtils;
+import com.kotcrab.vis.ui.FocusManager;
+import com.kotcrab.vis.ui.util.Validators;
 import com.kotcrab.vis.ui.widget.VisLabel;
 import com.kotcrab.vis.ui.widget.VisTextButton;
 import com.kotcrab.vis.ui.widget.VisTextField;
+import com.kotcrab.vis.ui.widget.VisValidatableTextField;
 
 /**
  * Project: impulse
@@ -22,14 +25,14 @@ public class MainGameScreen extends ImpulseScreenAdapter {
     Integer num = -1;
 
     private final VisTextButton back_btn;
-    private final VisTextField input_text;
+    private final VisValidatableTextField input_text;
     private final VisLabel output_text;
 
     public MainGameScreen(Game g) {
         super(g, "scene/main_game.scene");
 
         back_btn = new VisTextButton("BACK");
-        input_text = new VisTextField();
+        input_text = new VisValidatableTextField(Validators.INTEGERS);
         output_text = new VisLabel("");
     }
 
@@ -41,7 +44,7 @@ public class MainGameScreen extends ImpulseScreenAdapter {
         root.row();
         root.add(back_btn);
         root.row();
-        root.add(input_text);
+        root.add(input_text).width(400);
         root.row();
         root.add(output_text);
 
@@ -59,15 +62,15 @@ public class MainGameScreen extends ImpulseScreenAdapter {
             public boolean keyUp(InputEvent event, int keycode) {
                 switch (keycode) {
                     case Input.Keys.ENTER:
-                        String text = ((VisTextField) (event.getTarget())).getText();
-                        boolean valid = StringUtils.isValidInteger(text);
+                        String text = ((VisValidatableTextField) (event.getTarget())).getText();
+                        boolean textbox_valid = ((VisValidatableTextField) event.getTarget()).isInputValid();
 
-                        if(valid) {
+                        if(textbox_valid) {
                             num = Integer.parseInt(text);
                             output_text.setText("I can haz " + num + CONST.XI + "?");
                         }
-                        input_text.setInputValid(valid);
                     case Input.Keys.ESCAPE:
+                        FocusManager.resetFocus(stage);
                         break;
                     default:
                         break;
